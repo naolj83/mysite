a9ee722fe8bc06969b9d8f7824c75d6662aa7348
BIT <BIT@DESKTOP-U4THS2M>
2021-05-27 13:57:45 +0900

    user #2

---
+++ mysite02/src/main/java/com/douzone/mysite/controller/UserController.java
@@ -0,0 +1,49 @@
+package com.douzone.mysite.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.douzone.mysite.mvc.util.MvcUtils;
+import com.douzone.mysite.repository.UserRepository;
+import com.douzone.mysite.vo.UserVo;
+
+public class UserController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		String action = request.getParameter("a");
+
+		if("joinform".equals(action)) {
+			MvcUtils.forward("user/joinform", request, response);
+		} else if("joinsuccess".equals(action)) {
+			MvcUtils.forward("user/joinsuccess", request, response);
+		} else if("join".equals(action)) {
+			String name = request.getParameter("name");
+			String email = request.getParameter("email");
+			String password = request.getParameter("password");
+			String gender = request.getParameter("gender");
+			
+			UserVo vo = new UserVo();
+			vo.setName(name);
+			vo.setEmail(email);
+			vo.setPassword(password);
+			vo.setGender(gender);
+			
+			new UserRepository().insert(vo);
+			MvcUtils.redirect(request.getContextPath() + "/user?a=joinsuccess", request, response);
+		} else {
+			MvcUtils.redirect(request.getContextPath(), request, response);
+		}
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
