27c5d5694349f8c836bf7c2cef9a2c550b95c30e
BIT <BIT@DESKTOP-U4THS2M>
2021-05-28 12:01:41 +0900

    update user#1

--- mysite02/src/main/java/com/douzone/mysite/mvc/user/LoginAction.java
+++ mysite02/src/main/java/com/douzone/mysite/mvc/user/LoginAction.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.douzone.mvc.Action;
 import com.douzone.mvc.util.MvcUtils;
@@ -26,8 +27,9 @@ public class LoginAction implements Action {
 			return;
 		}
 		
-		/* 인증처리(session 처리) */
-
+		/* 인증처리(session 처리) */						// false - 없으면 null, 있으면 있는거 줘
+		HttpSession session = request.getSession(true);	// 없으면 만들어서 주고 있으면 있는거 줘
+		session.setAttribute("authUser", userVo);		
 		
 		// main으로 리다이렉트!
 		MvcUtils.redirect(request.getContextPath(), request, response);
