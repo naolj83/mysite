f4c5e29e1a61a04d1210e49d7db71b50de0e5869
naolj83 <naolj83@gmail.com>
2021-06-04 09:46:03 +0900

    view resolver 설정

--- mysite03/src/main/java/com/douzone/mysite/controller/GuestbookController.java
+++ mysite03/src/main/java/com/douzone/mysite/controller/GuestbookController.java
@@ -24,13 +24,13 @@ public class GuestbookController {
 	public String index(Model model) {
 		List<GuestbookVo> list = guestbookService.getMessageList();
 		model.addAttribute("list", list);
-		return "/WEB-INF/views/guestbook/index.jsp";
+		return "guestbook/index";
 	}
 	
 	@RequestMapping(value="/delete/{no}", method=RequestMethod.GET)
 	public String delete(@PathVariable("no") Long no, Model model) {
 		model.addAttribute("no", no);
-		return "/WEB-INF/views/guestbook/deleteform.jsp";
+		return "guestbook/deleteform";
 	}
 
 	@RequestMapping(value="/add", method=RequestMethod.POST)
