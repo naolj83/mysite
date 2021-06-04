04f715b7596fbd77568c7113b42c22c1fa49fea8
naolj83 <naolj83@gmail.com>
2021-06-04 15:02:49 +0900

    MyBatis 설정 및 적용

--- mysite03/src/main/java/com/douzone/mysite/repository/GuestbookRepository.java
+++ mysite03/src/main/java/com/douzone/mysite/repository/GuestbookRepository.java
@@ -1,13 +1,16 @@
 package com.douzone.mysite.repository;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.douzone.mysite.exception.GuestbookRepositoryException;
@@ -15,58 +18,15 @@ import com.douzone.mysite.vo.GuestbookVo;
 
 @Repository
 public class GuestbookRepository {
+
+	@Autowired
+	private SqlSession sqlSession;
+
+	@Autowired
+	private DataSource datasource;
+	
 	public List<GuestbookVo> findAll() {
-		List<GuestbookVo> list = new ArrayList<>();
-		
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-				
-		try {
-			conn = getConnection();
-			
-			String sql =
-				"   select no, name, date_format(reg_date, '%Y/%m/%d %H:%i:%s') as reg_date, message" +
-				"     from guestbook" +
-				" order by reg_date desc";
-			pstmt = conn.prepareStatement(sql);
-			
-			rs = pstmt.executeQuery();
-			
-			while(rs.next()) {
-				Long no = rs.getLong(1);
-				String name = rs.getString(2);
-				String regDate = rs.getString(3);
-				String message = rs.getString(4);
-				
-				GuestbookVo vo = new GuestbookVo();
-				vo.setNo(no);
-				vo.setName(name);
-				vo.setRegDate(regDate);
-				vo.setMessage(message);
-				
-				list.add(vo);
-			}
-			
-		} catch (SQLException e) {
-			throw new GuestbookRepositoryException(e.getMessage());
-		} finally {
-			try {
-				if(rs != null) {
-					rs.close();
-				}
-				if(pstmt != null) {
-					pstmt.close();
-				}
-				if(conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-			throw new GuestbookRepositoryException(e.getMessage());
-			}
-		}
-		
-		return list;
+		return sqlSession.selectList("guestbook.findAll");
 	}
 	
 	public boolean delete(GuestbookVo vo) {
@@ -75,7 +35,7 @@ public class GuestbookRepository {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		try {
-			conn = getConnection();
+			conn = datasource.getConnection();
 			
 			String sql =
 					" delete" +
@@ -114,7 +74,7 @@ public class GuestbookRepository {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		try {
-			conn = getConnection();
+			conn = datasource.getConnection();
 			
 			String sql =
 					" insert" +
@@ -147,16 +107,4 @@ public class GuestbookRepository {
 		return result;
 	}
 	
-	private Connection getConnection() throws SQLException {
-		Connection conn = null;
-		try {
-			Class.forName("org.mariadb.jdbc.Driver");
-			String url = "jdbc:mysql://192.168.80.131:3307/webdb?characterEncoding=utf8";
-			conn = DriverManager.getConnection(url, "webdb", "webdb");
-		} catch (ClassNotFoundException e) {
-			System.out.println("드라이버 로딩 실패:" + e);
-		} 
-		
-		return conn;
-	}	
 }
